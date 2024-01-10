63c127c33faa5f4eb99aadd56394467b8d9cfff0
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2024-01-10 21:35:36 +0530

    fix: moongodb connection

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import dbconnection from './shared/moongodb.js';
+// import dbconnection from "./shared/moongod.js";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from 'mongoose';
 
 import userRoutes from './routes/userRoutes.js';
 import { mainEndpoint } from './shared/constant.js';
@@ -11,12 +12,17 @@ const app = express();
 app.use(cors());
 //configure env;
 dotenv.config();
-
+app.use(express.json());
 app.use(express.json());
 const PORT = process.env.PORT
-dbconnection()
-app.use(express.json());
-
+// dbconnection()
+mongoose.connect(process.env.DB_CONNECT)
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+      console.error('Error connecting to MongoDB:', error);
+    });
 app.use(mainEndpoint, userRoutes);
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}/`)
